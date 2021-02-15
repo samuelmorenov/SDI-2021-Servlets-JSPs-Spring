bf97e8215107c6e71e72dbe4cd57c773d345ec8d
samuelmorenov <xamelsamuel@gmail.com>
2021-02-15 12:41:24 +0100

    SDI-2021-1005-session2-Ejercicio-complementario3

--- sdi2021-1005-spring/src/main/java/com/uniovi/controllers/ProfessorsController.java
+++ sdi2021-1005-spring/src/main/java/com/uniovi/controllers/ProfessorsController.java
@@ -1,46 +1,49 @@
 package com.uniovi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.uniovi.entities.Professor;
 import com.uniovi.services.ProfessorService;
 
-@RestController
+@Controller
 public class ProfessorsController {
 
 	@Autowired
 	private ProfessorService professorService;
 
 	@RequestMapping("/professor/list")
-	public String getList() {
-		return professorService.getProfessors().toString();
+	public String getList(Model model) {
+		model.addAttribute("professorList", professorService.getProfessors());
+		return "professor/list";
 	}
 
 	@RequestMapping(value = "/professor/add", method = RequestMethod.POST)
-	public String setProfessor(@ModelAttribute Professor professor) {
+	public String addProfessor(@ModelAttribute Professor professor) {
 		professorService.addProfessor(professor);
-		return "Ok";
+		return "redirect:/professor/list";
 	}
 
-	@RequestMapping("/professor/add")
+	@RequestMapping(value = "/professor/add")
 	public String addProfessor() {
-		return "Formulario de para añadir";
+		return "professor/add";
 	}
 
-	@RequestMapping("/professor/details/{DNI}")
-	public String getProfessor(@PathVariable Long DNI) {
-		return professorService.getProfessor(DNI).toString();
+	@RequestMapping("/professor/details/{id}")
+	public String getProfessor(Model model, @PathVariable Long id) {
+		model.addAttribute("professor", professorService.getProfessor(id));
+		return "professor/details";
 	}
 
-	@RequestMapping("/professor/delete/{DNI}")
-	public String deleteProfessor(@PathVariable Long DNI) {
-		professorService.deleteProfessor(DNI);
-		return "Ok";
+	@RequestMapping("/professor/delete/{id}")
+	public String deleteProfessor(@PathVariable Long id) {
+		professorService.deleteProfessor(id);
+		return "redirect:/professor/list";
 	}
 
 }
