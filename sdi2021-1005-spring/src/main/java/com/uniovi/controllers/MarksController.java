5ab3ec97494d9f6beae182cb937eddfb3ce106df
samuelmorenov <xamelsamuel@gmail.com>
2021-02-27 20:07:43 +0100

    SDI-2021-1005-4.4-Consulta.

--- sdi2021-1005-spring/src/main/java/com/uniovi/controllers/MarksController.java
+++ sdi2021-1005-spring/src/main/java/com/uniovi/controllers/MarksController.java
@@ -1,6 +1,10 @@
 package com.uniovi.controllers;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.uniovi.entities.Mark;
+import com.uniovi.entities.User;
 import com.uniovi.services.MarksService;
 import com.uniovi.services.UsersService;
 import com.uniovi.validators.AddMarkValidator;
@@ -27,14 +32,19 @@ public class MarksController {
 	private AddMarkValidator addMarkValidator;
 
 	@RequestMapping("/mark/list/update")
-	public String updateList(Model model) {
-		model.addAttribute("markList", marksService.getMarks());
+	public String updateList(Model model, Principal principal) {
+		String dni = principal.getName(); // DNI es el name de la autenticación
+		User user = usersService.getUserByDni(dni);
+		model.addAttribute("markList", marksService.getMarksForUser(user));
 		return "mark/list :: tableMarks";
 	}
 
 	@RequestMapping("/mark/list")
 	public String getList(Model model) {
-		model.addAttribute("markList", marksService.getMarks());
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String dni = auth.getName();
+		User user = usersService.getUserByDni(dni);
+		model.addAttribute("markList", marksService.getMarksForUser(user));
 		return "mark/list";
 	}
 
